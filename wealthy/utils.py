b12b7543934964b749ccb2b778fcd75759763faa
Travis <travis6888@yahoo.com>
2014-10-17 15:11:57 -0700

    cleaning views, refactoring

--- wealthy/utils.py
+++ wealthy/utils.py
@@ -1,1 +1,23 @@
+import numpy
+
 __author__ = 'Travis'
+
+
+def demo_age_calc(age):
+    percent_month = (float(4000) * .60)
+    investment_month = (float(4000) - float(percent_month))
+    inv_return = numpy.fv((.058 / 12), ((65 - (int(age))) * 12), -(int(investment_month)), -investment_month)
+    investment_return = '{:20,.2f}'.format(float(inv_return))
+    vistior_age = {'investment': investment_month, 'age': age, 'return': investment_return,
+                   "percent_mon": percent_month}
+    return vistior_age
+
+
+def find_invest_month_calc(investor, monies):
+    percent_month = (float(monies) * .60)
+    investment_month = (float(monies) - float(percent_month))
+    investor.monthly_investment = investment_month
+    investor.save()
+    investment_clean = '{:20,.2f}'.format(investment_month)
+    investing = {'invest': investment_clean}
+    return investing
