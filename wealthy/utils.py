995f5b51c7395a56145878f1e3de8fdcf59a4408
Travis <travis6888@yahoo.com>
2014-10-22 19:03:05 -0700

    minor spelling tweaks

--- wealthy/utils.py
+++ wealthy/utils.py
@@ -32,7 +32,7 @@ def input_income_calc(investor, income_input, taxes):
 
 
 def portfolio_return_calc(age, investment, risk_portfolio ):
-    portfolio_attr= []
+    portfolio_attr = []
     portfolio_list = Portfolio.objects.filter(name=risk_portfolio)
     stock_list = Investment.objects.filter(portfolios__name=risk_portfolio)
     stock_name = []
@@ -42,19 +42,15 @@ def portfolio_return_calc(age, investment, risk_portfolio ):
         stock_description = {
                 'name': stock.name,
                 'info': stock.description}
-
         stock_info_list.append(stock_description)
     print stock_info_list
     for portfolio in portfolio_list:
         portfolio_attr.append(portfolio.name)
         portfolio_attr.append(portfolio.expected_return)
-        print portfolio.investments
-        print portfolio.expected_return
     inv_return = numpy.fv((int(portfolio_attr[1]) / 12), ((65 - (int(age))) * 12), -(int(investment)), -investment)
     investment_return = '{:20,.2f}'.format(float(inv_return))
-
     data2 = {'stocksp': {'stock1p': 25, 'stock2p': 25, 'stock3p': 20, 'stock4p': 20, 'stock5p': 10},
-                     'stocksn': {'stock1n': stock_name[0], 'stock2n': stock_name[1], 'stock3n': stock_name[2], 'stock4n': stock_name[3], 'stock5n': stock_name[4]},
+                     'stocksn': {'stock1n': stock_name[0], 'stock2n': stock_name[1], 'stock3n': stock_name[2], 'stock4n':stock_name[3], 'stock5n': stock_name[4]},
                     'portfolio': portfolio_attr[0],
                     'expected': portfolio_attr[1],
                     'return': investment_return, 'stock_list': stock_info_list}
