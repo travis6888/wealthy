535226d00e91a8306163fd4dee9058a2f6d15686
Travis <travis6888@yahoo.com>
2014-11-16 12:40:35 -0800

    symbol lookup

--- advisor/static/js/dashboard.js
+++ advisor/static/js/dashboard.js
@@ -17,7 +17,24 @@ $('.loadPort').on('click', function(){
         error: function(error){
             console.log(error)
         }
-    })
+    });
+    var stocks =  {'names': []};
+    $.ajax({
+            url: '/find_portfolio/',
+            type: 'GET',
+            dataType: 'json',
+
+            success: function (stock_response) {
+                var stockedname = stock_response.stocksn;
+                stocks['names'].push(stockedname.stock1n, stockedname.stock2n, stockedname.stock3n, stockedname.stock4n, stockedname.stock5n);
+
+            },
+            error: function (error) {
+                console.log(error);
+
+            }
+
+            });
 });
 
 
