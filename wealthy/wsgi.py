e492d274e4ab615bd9366ded62475d70f1e591ee
Travis <travis6888@yahoo.com>
2014-12-14 14:48:59 -0800

    new relic

--- wealthy/wsgi.py
+++ wealthy/wsgi.py
@@ -6,8 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import newrelic.agent
-newrelic.agent.initialize('newrelic.ini')
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wealthy.settings")
 
