4638281db8b5f089a53b7b025ba06a862f73bafe
Travis <travis6888@yahoo.com>
2015-01-29 21:50:55 -0800

    begin blog app

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,27 @@ from django.db import models
 from advisor.models import Investor
 
 
-class Blog(models.Model):
-    title = models.CharField(max_length=100, blank=True, null=True)
-    meta = models.TextField(max_length=1000, blank=True, null=True)
-    content = models.TextField(max_length=12000, blank=True, null=True)
-    author = models.ForeignKey(Investor, blank=True, null=True)
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    bio = models.TextField()
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+
+class Post(models.Model):
+    created = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=120)
+    body = models.TextField()
+    author = models.ForeignKey(Author, related_name="posts")
+    tags = models.ManyToManyField(Tag, related_name="posts")
+
+    def __unicode__(self):
+        return u"{}".format(self.title)
